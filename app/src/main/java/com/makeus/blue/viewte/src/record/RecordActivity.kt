5d91c0e198104e5be060787fa060ab4e7d704785
CWonLee <46565404+CWonLee@users.noreply.github.com>
2020-10-19 06:02:47 +0900

    음성인식

--- app/src/main/java/com/makeus/blue/viewte/src/record/RecordActivity.kt
+++ app/src/main/java/com/makeus/blue/viewte/src/record/RecordActivity.kt
@@ -1,31 +1,35 @@
 package com.makeus.blue.viewte.src.record
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.text.method.ScrollingMovementMethod
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
-import com.kakao.sdk.newtoneapi.SpeechRecognizeListener
-import com.kakao.sdk.newtoneapi.SpeechRecognizerClient
-import com.kakao.sdk.newtoneapi.SpeechRecognizerManager
+import androidx.core.content.ContextCompat
 import com.makeus.blue.viewte.R
 import com.makeus.blue.viewte.src.BaseActivity
-import com.vikramezhil.droidspeech.DroidSpeech
-import com.vikramezhil.droidspeech.OnDSListener
 
 
-class RecordActivity : BaseActivity(), OnDSListener {
+class RecordActivity : BaseActivity(), RecognitionListener {
 
     private val REQUEST_CODE_AUDIO_AND_WRITE_EXTERNAL_STORAGE: Int = 10000
+    private val PERMISSIONS_REQUEST_RECORD_AUDIO = 1
     private lateinit var mClStart: ConstraintLayout
     private lateinit var mEtSTT: EditText
     var play: Boolean = false
-    var resultString: String = ""
+    var mResultString: String = ""
+    private var speech: SpeechRecognizer? = null
+    private var recognizerIntent: Intent? = null
+    private val LOG_TAG = "VoiceRecognitionActivity"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,80 +38,117 @@ class RecordActivity : BaseActivity(), OnDSListener {
         mClStart = findViewById(R.id.record_cl_start)
         mEtSTT = findViewById(R.id.record_et)
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO) && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_CODE_AUDIO_AND_WRITE_EXTERNAL_STORAGE)
-            }
-            else {
-                showCustomToast("권한이 없습니다")
-            }
+        // start speech recogniser
+        resetSpeechRecognizer()
+
+        // check for permission
+
+        // check for permission
+        val permissionCheck = ContextCompat.checkSelfPermission(
+            applicationContext,
+            Manifest.permission.RECORD_AUDIO
+        )
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                PERMISSIONS_REQUEST_RECORD_AUDIO
+            )
+            return
         }
-        else {
-            startUsingSpeechSDK()
-        }
+
+        setRecogniserIntent();
+        speech!!.startListening(recognizerIntent)
+    }
+
+    @SuppressLint("LongLogTag")
+    private fun resetSpeechRecognizer() {
+        if (speech != null) speech!!.destroy()
+        speech = SpeechRecognizer.createSpeechRecognizer(this)
+        Log.i(
+            LOG_TAG,
+            "isRecognitionAvailable: " + SpeechRecognizer.isRecognitionAvailable(this)
+        )
+        if (SpeechRecognizer.isRecognitionAvailable(this))
+            speech!!.setRecognitionListener(this)
+        else finish()
+    }
+
+    private fun setRecogniserIntent() {
+        recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        recognizerIntent!!.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE,
+            "en"
+        )
+        recognizerIntent!!.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        recognizerIntent!!.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        when (requestCode) {
-            REQUEST_CODE_AUDIO_AND_WRITE_EXTERNAL_STORAGE -> {
-                if (grantResults.size > 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                    startUsingSpeechSDK()
-                } else {
-                    finish()
-                }
+        if (requestCode == PERMISSIONS_REQUEST_RECORD_AUDIO) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                speech!!.startListening(recognizerIntent);
+            } else {
+                showCustomToast("Permission Denied!")
+                finish();
             }
         }
     }
 
-
-    private fun startUsingSpeechSDK(){
-        //SDK 초기화
-        SpeechRecognizerManager.getInstance().initializeLibrary(this)
-        val droidSpeech = DroidSpeech(this, null)
-        droidSpeech.setOnDroidSpeechListener(this)
-
-        mClStart.setOnClickListener(object : OnSingleClickListener() {
-            override fun onSingleClick(v: View) {
-                if (play) {
-                    showCustomToast("음성인식 끝")
-                    play = false
-                    droidSpeech.closeDroidSpeechOperations()
-                }
-                else {
-                    showCustomToast("음성인식 시작")
-                    play = true
-                    droidSpeech.startDroidSpeechRecognition()
-                }
-            }
-        })
-    }
-
-    override fun onDroidSpeechRmsChanged(rmsChangedValue: Float) {
+    override fun onReadyForSpeech(params: Bundle?) {
 
     }
 
-    override fun onDroidSpeechSupportedLanguages(
-        currentSpeechLanguage: String?,
-        supportedSpeechLanguages: MutableList<String>?
-    ) {
+    override fun onRmsChanged(rmsdB: Float) {
 
     }
 
-    override fun onDroidSpeechError(errorMsg: String?) {
+    override fun onBufferReceived(buffer: ByteArray?) {
 
     }
 
-    override fun onDroidSpeechClosedByUser() {
+    override fun onPartialResults(partialResults: Bundle?) {
+        val matches = partialResults
+            ?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+        var text = ""
+        text = matches!![0]
+
+        println(text)
+        mEtSTT.setText(mResultString + text)
     }
 
-    override fun onDroidSpeechLiveResult(liveSpeechResult: String?) {
-        mEtSTT.setText(resultString + liveSpeechResult)
+    override fun onEvent(eventType: Int, params: Bundle?) {
+
     }
 
-    override fun onDroidSpeechFinalResult(finalSpeechResult: String?) {
-        resultString += finalSpeechResult + "\n"
-        mEtSTT.setText(resultString)
+    override fun onBeginningOfSpeech() {
+
+    }
+
+    override fun onEndOfSpeech() {
+        speech!!.stopListening()
+    }
+
+    override fun onError(error: Int) {
+        // rest voice recogniser
+        resetSpeechRecognizer();
+        speech!!.startListening(recognizerIntent);
+    }
+
+    override fun onResults(results: Bundle?) {
+        val matches = results
+            ?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+        var text = matches!![0]
+
+
+        println(text)
+        mResultString += text
+        mEtSTT.setText(mResultString)
+        speech!!.startListening(recognizerIntent)
     }
 }

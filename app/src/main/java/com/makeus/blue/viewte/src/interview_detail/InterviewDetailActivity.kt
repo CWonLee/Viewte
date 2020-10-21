239b2e1fa1a92d083e05a93b2d28586b725c5136
CWonLee <46565404+CWonLee@users.noreply.github.com>
2020-10-22 06:38:36 +0900

    interview detail

---
+++ app/src/main/java/com/makeus/blue/viewte/src/interview_detail/InterviewDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.makeus.blue.viewte.src.interview_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.makeus.blue.viewte.R
+
+class InterviewDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_interview_detail)
+    }
+}
